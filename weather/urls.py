2ae26df0fb3f15ac1dde5f56e3de032e15870d93
khalimanov <khalimanov@gmail.com>
2016-02-05 21:48:30 +1000

    Добавлена модель с температурой, добавление в бд, страница со списком датчиков

---
+++ weather/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    # ex: /weather/ список датчиков
+    url(r'^$', views.index, name='index'),
+    # ex: /weather/1/ возврат значения температуры не работает
+    #url(r'^(?P<sensor_id>[0-9]+)/$', views.sensorname, name='sensorname'),
+    # ex: /weather/results/ значения температуры не работает
+    #url(r'^results/$', views.results, name='results'),
+    # ex: /weather/addvalue/ добавление значения
+    url(r'^addvalue/$', views.addvalue, name='addvalue'),
+]
--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Sensor, Values
+from django.views.decorators.csrf import csrf_exempt # Чтобы работал тест запросов
 
-# Create your views here.
+
+def index(request):
+    sensor_list = Sensor.objects.all()
+    context = {'sensor_list': sensor_list}
+    return render(request, 'weather/index.html', context)
+
+
+@csrf_exempt
+def addvalue(request):
+    if request.method != 'POST':
+        return HttpResponse('request need to be POST')
+    sensor_id = request.POST.get("sensor_id", 0)
+    temp = request.POST.get('temp', 0)
+    hum = request.POST.get('hum', 0)
+    if sensor_id == 0 or hum == 0:
+        return HttpResponse('Bad info')
+    sensor = Sensor.objects.get(sensor_id_const=sensor_id)
+    new_value = Values()
+    new_value.sensor = sensor
+    new_value.temp = temp
+    new_value.hum = hum
+    new_value.save()
+    return HttpResponse('200 OK')
